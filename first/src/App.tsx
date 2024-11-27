000e4e37070cbc923e520eed567a571d30542f48
HP <amxdhnanditha@gmail.com>
2024-11-27 12:06:15 +0530

    append component with array and list

--- first/src/App.tsx
+++ first/src/App.tsx
@@ -1,3 +1,35 @@
+// import './App.css'
+// import Item from "./component/Item.tsx";
+//
+// function App() {
+//
+//     const itemList = [
+//         {
+//             title:"AAA",
+//             description:"sda"
+//         },
+//         {
+//             title:"BBB",
+//             description:"sda"
+//         },
+//         {
+//             title:"CCC",
+//             description:"sda"
+//         }
+//     ]
+//
+//   return (
+//     <>
+//         <Item title={itemList[0].title} description={itemList[0].description}/>
+//        <Item{...itemList[0]}/>
+//        <Item{...itemList[1]}/>
+//        <Item{...itemList[2]}/>
+//     </>
+//   )
+// }
+//
+// export default App
+
 import './App.css'
 import Item from "./component/Item.tsx";
 
@@ -18,14 +50,25 @@ function App() {
         }
     ]
 
-  return (
-    <>
-        <Item title={itemList[0].title} description={itemList[0].description}/>
-       <Item{...itemList[0]}/>
-       <Item{...itemList[1]}/>
-       <Item{...itemList[2]}/>
-    </>
-  )
+    const itemsArray = []
+    for (const item of itemList) {
+        itemsArray.push(
+            <Item title={item.title} description={item.description} />
+        );
+    }
+
+
+    return (
+        <>
+            {/*Way one*/}
+            {itemList.map((item, ) => (
+                <Item title={item.title} description={item.description}/>
+            ))}
+
+            {/*Way two*/}
+            {itemsArray}
+        </>
+    )
 }
 
 export default App
