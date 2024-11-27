8aa0278e28d33998b58ccb9401b10825d4b3d9f3
HP <amxdhnanditha@gmail.com>
2024-11-27 10:45:36 +0530

    initial commit

---
+++ first/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Item from "./component/Item.tsx";
+
+function App() {
+
+  return (
+    <>
+       <Item title={"HELLO MAN"} description={"lorem lorem"}/>
+    </>
+  )
+}
+
+export default App
---
+++ first/src/component/Item.tsx
@@ -0,0 +1,16 @@
+// export default function Item(props :{title:string,description:string}) {
+//     return(
+//         <>
+//             <h5>{props.title}</h5>
+//             <p>{props.description}</p>
+//         </>
+//     )
+// }
+export default function Item({title,description}: {title: string; description: string}) {
+    return(
+        <>
+            <h5>{title}</h5>
+            <p>{description}</p>
+        </>
+    )
+}
